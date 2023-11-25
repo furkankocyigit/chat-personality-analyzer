99a52f092a61236d57aed2da613c78b273a7ab66
Furkan <furkankocyigit58@gmail.com>
2023-11-25 20:36:16 +0300

    summarized evaluation is implemented in evauationService

--- src/domain.model/EvaluationFormat.ts
+++ src/domain.model/EvaluationFormat.ts
@@ -23,3 +23,29 @@ export const EvaluationFormat = {
     ],
     evaluationDescriptions: 'Example summary of the evaluation less than 50 words',
 };
+
+export const MockEvaluationResult = {
+    personalities: [
+        { personality: 'Empathetic', score: 0.2 },
+        { personality: 'Patient', score: 0.7 },
+        { personality: 'Aggressive', score: 0.4 },
+        { personality: 'Manipulative', score: 0.6 },
+        { personality: 'OpenMinded', score: 0.9 },
+        { personality: 'Reliable', score: 0.4 },
+        { personality: 'Supportive', score: 0.3 },
+        { personality: 'Confident', score: 0.2 },
+        { personality: 'Loyal', score: 0.1 },
+        { personality: 'Constructive', score: 0.1 },
+        { personality: 'Respectful', score: 0.8 },
+        { personality: 'Needy', score: 0.7 },
+        { personality: 'Stubborn', score: 0.6 },
+        { personality: 'Selfish', score: 0.0 },
+        { personality: 'Insecure', score: 0.8 },
+        { personality: 'Insensitive', score: 0.3 },
+        { personality: 'Impatient', score: 0.2 },
+        { personality: 'Impulsive', score: 0.8 },
+        { personality: 'Egotistical', score: 0.9 },
+        { personality: 'Honest', score: 0.8 },
+    ],
+    evaluationDescriptions: 'Example summary of the evaluation less than 50 words',
+};

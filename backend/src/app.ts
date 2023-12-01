69c0e14b0dcf5d62244217f5b81048b7aef98503
Furkan <furkankocyigit58@gmail.com>
2023-12-01 14:26:56 +0300

    cors is added to backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,19 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import routes from './application/routes';
 import { ErrorHandlingMiddleware } from './utils/ErrorHandler';
 
 const app = express();
+
+// enable cors
+app.use((req: Request, res: Response, next: NextFunction): void => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization, x-api-key'
+    );
+    next();
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));

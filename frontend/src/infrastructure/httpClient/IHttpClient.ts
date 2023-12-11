fb9a26a2dc512602e71e025b78814e7d3dc96c6a
Furkan <furkankocyigit58@gmail.com>
2023-12-11 21:04:59 +0300

    get all aded to IHttpClient

--- frontend/src/infrastructure/httpClient/IHttpClient.ts
+++ frontend/src/infrastructure/httpClient/IHttpClient.ts
@@ -1,5 +1,6 @@
 export interface IHttpClient {
     get(url: string): Promise<unknown>;
+    getAll(url: string): Promise<unknown[]>;
     post(url: string, body: unknown): Promise<unknown>;
     put(url: string, body: unknown): Promise<unknown>;
     delete(url: string): Promise<unknown>;
